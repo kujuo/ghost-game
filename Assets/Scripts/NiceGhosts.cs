fa60a32a25eae526e4fe4f997ed43497e9e3575a
ngocdiep892000 <diephoang@8200D-0786.DisStu.local>
2022-09-12 14:12:01 +0200

    Revert "hearts and shooty"
    This reverts commit 01424ec99ff157f6f3c798bcf4f6ad1d9dba4015.

--- Assets/Scripts/NiceGhosts.cs
+++ Assets/Scripts/NiceGhosts.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class NiceGhosts : MonoBehaviour
+public class NiceGhosts : MonoBehaviour
 {
-    public abstract void ApplySkill();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
-    
