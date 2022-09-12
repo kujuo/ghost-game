5110bf800ebcaf3f7574800d0aa01a41988aa76e
ngocdiep892000 <diephoang@8200D-0786.DisStu.local>
2022-09-12 15:47:28 +0200

    nice ghost shooting

--- Assets/Scripts/NiceGhosts.cs
+++ Assets/Scripts/NiceGhosts.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public abstract class NiceGhosts : MonoBehaviour
 {
+    public GameObject player;
+
+    private void Start()
+    {
+        transform.SetParent(player.transform);
+        transform.position = transform.position + new Vector3(-5, 1, 0) * 0.5f;
+    }
     public abstract void ApplySkill();
 
     private void Update()
